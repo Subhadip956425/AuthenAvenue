d65d8534a3f7751e64b9b9bf6d58c045b3f89323
Subhadip Guchhait <subhadipguchhait106@gmail.com>
2025-07-22 22:43:27 +0530

    Update env.js Update backend host url

--- trading-react/src/State/Enviorenment/env.js
+++ trading-react/src/State/Enviorenment/env.js
@@ -1,2 +1,4 @@
 // src/config.js or wherever you like
-export const frontend_baseurl = "http://localhost:5454";
+// export const frontend_baseurl = "http://localhost:5454";
+
+export const frontend_baseurl = "https://authenavenue.onrender.com"
